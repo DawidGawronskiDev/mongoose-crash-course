a826342b8edb7b792cce68667c1fc9d4736d35d3
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-02-05 12:30:27 +0000

    Create new user

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose")
+const User = require("./User")
 
 mongoose.connect("mongodb://localhost/testdb")
+
+async function run() {
+    const user = new User({ name: "Kyle", age: 26 })
+    await user.save()
+    console.log(user)
+}
+run()
